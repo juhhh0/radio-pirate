5cc9ae2aeea46dd21f20429abcae316e32b2ab23
Joris Mansion <jormansi@WKMZT57714E3.global.publicisgroupe.net>
2023-12-04 20:16:12 +0100

    update urls

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ export default function Navbar({setRadio}){
         <nav>
         <h1>pirate_radio</h1>
         <ul>
-          <NavbarItem setRadio={setRadio} name="water-lily.mp3" url="https://radio-pirate.com/water" />
-          <NavbarItem setRadio={setRadio} name="tamagucci.ogg" url="https://radio-pirate.com/tamagucci" />
-          <NavbarItem setRadio={setRadio} name="ok.wav" url="https://radio-pirate.com/ok" />
+          <NavbarItem setRadio={setRadio} name="water-lily.mp3" url="https://ice.radio-pirate.com/water" />
+          <NavbarItem setRadio={setRadio} name="tamagucci.ogg" url="https://ice.radio-pirate.com/tamagucci" />
+          <NavbarItem setRadio={setRadio} name="ok.wav" url="https://ice.radio-pirate.com/ok" />
         </ul>
       </nav>
     )   
