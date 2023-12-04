7be1fa971e16114c2f383e71327b39addf5dd810
Joris Mansion <jormansi@WKMZT57714E3.global.publicisgroupe.net>
2023-12-04 20:18:23 +0100

    update title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import NavbarItem from "./NavbarItem";
 export default function Navbar({setRadio}){
     return (
         <nav>
-        <h1>pirate_radio</h1>
+        <h1>radio_pirate</h1>
         <ul>
           <NavbarItem setRadio={setRadio} name="water-lily.mp3" url="https://ice.radio-pirate.com/water" />
           <NavbarItem setRadio={setRadio} name="tamagucci.ogg" url="https://ice.radio-pirate.com/tamagucci" />
