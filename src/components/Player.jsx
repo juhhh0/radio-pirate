5dc5af69f23c785258008b4a0521bdf7170ea635
Joris Mansion <jormansi@WKMZT57714E3.global.publicisgroupe.net>
2023-12-04 23:46:52 +0100

    song title

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,32 +1,69 @@
-import { useRef } from "react"
+import { useRef } from "react";
 
-import play from "../assets/play.svg"
-import pause from "../assets/pause.svg"
-import { useState } from "react"
-import { useEffect } from "react"
-import ThreeCube from "./ThreeCube"
+import play from "../assets/play.svg";
+import pause from "../assets/pause.svg";
+import { useState } from "react";
+import { useEffect } from "react";
+import ThreeCube from "./ThreeCube";
 
-export default function Player({radio}){
-    const [playing, setPlaying] = useState(false)
-    const audio = useRef(null)
+export default function Player({ radio }) {
+  const [playing, setPlaying] = useState(false);
+  const audio = useRef(null);
+  const [data, setData] = useState({})
 
-    const toggleAudio = () => {
-        audio.current.paused ? audio.current.play() : audio.current.pause()
-        setPlaying(prev => !prev)
-        
+  const toggleAudio = () => {
+    audio.current.paused ? audio.current.play() : audio.current.pause();
+    setPlaying((prev) => !prev);
+  };
+
+  useEffect(() => {
+    setPlaying(false);
+  }, [radio]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        console.log("fetch !")
+      try {
+        const response = await fetch("https://ice.radio-pirate.com/status-json.xsl");
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        setData(data.icestats.source)
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+
+    const refetch = setInterval(fetchData, 10000);
+
+    return () => clearInterval(refetch);
+
+  }, []);
+
+  let song = ''
+
+  if(data.length > 0){
+       song = data.find(item => item.server_name === radio.url).title
     }
 
-    useEffect(() => {
-        setPlaying(false)
-    }, [radio])
-    return (
-        <>
-        <section>
-            <audio ref={audio} src={`https://ice.radio-pirate.com/${radio.url}`}></audio>
-            <h2>{radio.name}</h2>
-            <ThreeCube/>
-           <img onClick={toggleAudio} src={playing? pause : play} />
-        </section>
-        </>
-    )
+  return (
+    <>
+      <section>
+        <audio
+          ref={audio}
+          src={`https://ice.radio-pirate.com/${radio.url}`}
+        ></audio>
+        <h2>{radio.name}</h2>
+        <ThreeCube />
+        <p>{song}</p>
+        <img onClick={toggleAudio} src={playing ? pause : play} />
+      </section>
+    </>
+  );
 }
