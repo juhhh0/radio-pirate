d1d2c9e1d34605bf46a396186227f7d2216bdc54
Joris Mansion <jormansi@WKMZT57714E3.global.publicisgroupe.net>
2023-12-04 20:41:20 +0100

    🐝

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 import Player from "./components/Player"
 
 function App() {
-  const [radio, setRadio] = useState({name: "water-lily.mp3", url: "https://ice.radio-pirate.com/water"})
+  const [radio, setRadio] = useState({name: "water-lily.mp3", url: "water"})
   return (
     <>
     <main>
