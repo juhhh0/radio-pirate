f2d11d1cdbc82ad65bdb996cb5a996988ee90d97
Joris Mansion <juhh@juhh-core.lan>
2024-03-13 22:22:25 +0100

    encore logo

--- src/components/ThreeCube.jsx
+++ src/components/ThreeCube.jsx
@@ -1,13 +1,15 @@
 import React, { useRef } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Canvas, useFrame, useThree, useLoader } from "@react-three/fiber";
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import { useEffect } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const Cube = () => {
   const cubeRef = useRef();
+  const gltf = useLoader(GLTFLoader, "/radio.gltf");
 
-  const { playing, radio, setPlaying } = useContext(AppContext);
+  const { playing, radio } = useContext(AppContext);
 
   useFrame(() => {
     if (playing) {
@@ -31,34 +33,40 @@ const Cube = () => {
     }
   });
 
-
-  useThree(({camera}) => {
+  useThree(({ camera }) => {
     camera.position.z = 3;
   });
 
   return (
-    <mesh
-      ref={cubeRef}
-      rotation={[Math.PI / 6, Math.PI / 6, 0]}
-      onClick={() => {
-        setPlaying((prev) => !prev);
-      }}
-      onPointerOver={() => {
-        document.body.style.cursor = `pointer`;
-      }}
-      onPointerOut={() => {
-        document.body.style.cursor = 'auto';
-      }}
-    >
-      <boxGeometry args={[1, 2, 1]} />
-      <meshBasicMaterial color={0x000000} />
-    </mesh>
+    // <mesh
+    //   ref={cubeRef}
+    //   rotation={[Math.PI / 6, Math.PI / 6, 0]}
+    // onClick={() => {
+    //   setPlaying((prev) => !prev);
+    // }}
+    //   onPointerOver={() => {
+    //     document.body.style.cursor = `pointer`;
+    //   }}
+    //   onPointerOut={() => {
+    //     document.body.style.cursor = 'auto';
+    //   }}
+    // >
+    //   <boxGeometry args={[1, 2, 1]} />
+    //   <meshBasicMaterial color={0x000000} />
+    // </mesh>
+    <primitive ref={cubeRef} object={gltf.scene} scale={6} />
   );
 };
 
 const ThreeCube = () => {
+  const {setPlaying} = useContext(AppContext)
   return (
-    <Canvas className="canvas">
+    <Canvas
+      className="canvas"
+      onClick={() => {
+        setPlaying((prev) => !prev);
+      }}
+    >
       <Cube />
     </Canvas>
   );
